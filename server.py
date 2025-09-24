98c0afde76ddb85ccd4bc7b16a61fbafd73c020f
ladvllee-prog <ladvllee@gmail.com>
2025-09-24 23:47:38 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import threading
+import main
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bots are running!"
+
+def run_bots():
+    main.main()
+
+if __name__ == "__main__":
+    # Lancer les bots dans un thread
+    threading.Thread(target=run_bots, daemon=True).start()
+    # Lancer un mini serveur web pour Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
